e83d104dbdb52d95413fbaa45ec8001c2c481578
Hasan Zahid <hasanthedev@gmail.com>
2018-06-16 22:34:44 -0500

    navigation between screens, ++header/header style

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
-import {  View, Text, } from 'react-native';
+import { View, Text, } from 'react-native';
 
 export default class ListItem extends Component {
+  static navigationOptions = {
+    title: 'Images',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
   render() {
     return (
       <View>
--- app/containers/ListContainer.js
+++ app/containers/ListContainer.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import {  View, Text, } from 'react-native';
+import { View } from 'react-native';
+import { Button } from 'react-native-elements';
 import ListItem from '../components/ListItem';
-  
+
 console.ignoredYellowBox = ['Setting a timer'];
 
 export default class ListContainer extends Component {
-  
+  static navigationOptions = {
+    // making it so that this screen does not have a header
+    header: null
+  }
+
   render() {
     return (
-      <View>
-        <ListItem text={"Hello there"}/>
+      <View style={{ marginTop: '20%', justifyContent: 'center', }}>
+        <Button
+          large
+          backgroundColor='#f4511e'
+          buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+          title="Navigate!"
+          onPress={() => this.props.navigation.navigate('Images')}
+        />
       </View>
     );
   }
