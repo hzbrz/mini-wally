d4fb1b5f5f30c2b658098d5b74a46c125e5872a9
Hasan Zahid <hasanthedev@gmail.com>
2018-06-16 22:14:58 -0500

    setup navigation w/ setting up screens

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ListConatainer from './app/containers/ListContainer'
+import { StyleSheet } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import ListConatainer from './app/containers/ListContainer';
+import ListItem from './app/components/ListItem';
+
+const RootStack = createStackNavigator(
+  {
+    Cards: ListConatainer,
+    Images: ListItem,
+    // preview screen is not used but navigation is set up in case
+    // ImagePreview: Preview
+  },
+  {
+    initialRouteName: 'Cards',
+  }
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <ListConatainer />
-      </View>
+      <RootStack />
     );
   }
 }
--- app/containers/ListContainer.js
+++ app/containers/ListContainer.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import {  View, Text, } from 'react-native';
 import ListItem from '../components/ListItem';
-import { db_value, db_images_value } from '../ListFromFb';
-
+  
 console.ignoredYellowBox = ['Setting a timer'];
 
 export default class ListContainer extends Component {
   
-  componentDidMount() {
-    db_images_value()
-  }
-
   render() {
     return (
       <View>
